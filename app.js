133ff06b4437efd7a4f0baeec8e77b1b96a788d0
1085641510 <1085641510@qq.com>
2018-08-10 15:17:36 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
+
 const bodyParser = require('koa-bodyparser');
+const koaStatic = require('koa-static');
+const path = require('path');
+
 app.use(bodyParser());
 
+var projectDir = path.resolve('D:/project/wangpengqiang_iaf/iaf_om_console');
+app.use(koaStatic(projectDir));
 
 const router = require('./router.js');
 
--- router.js
+++ router.js
@@ -2,7 +2,9 @@ const Router = require('koa-router');
 const router = new Router();
 
 const login = require('./router/login.js');
+const index = require('./router/index.js');
+
 
 router.use(login.routes());
-
+router.use(index.routes());
 module.exports = router;
