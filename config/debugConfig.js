bc5d80aaa39db88bd90714d75b18ecb09036b6cd
1085641510 <1085641510@qq.com>
2018-08-10 15:15:25 +0800

    Add files via upload

---
+++ config/debugConfig.js
@@ -0,0 +1,5 @@
+const debug = require('debug');
+
+//debug.enable("app:*");
+//debug.log = console.log.bind(console); // 会将日志打印到debug.log文件中
+module.exports = debug;
--- config/serverConfig.js
+++ config/serverConfig.js
@@ -2,6 +2,6 @@ let path = require('path');
 
 module.exports = {
     env: 'test',        //环境名称
-    port: 3000,         //服务端口号
+    port: 3001,         //服务端口号
     root: path.resolve(__dirname, '')
 }
